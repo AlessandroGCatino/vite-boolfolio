b07ad51dafc064fc0a181ba8701fc068623f82e6
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-04-11 19:54:31 +0200

    creata pagina con import componente form

---
+++ src/pages/ContactUs.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        
+        <ContactForm/>
+
+    </div>
+</template>
+
+<script>
+    import ContactForm from '../components/ContactForm.vue';
+
+    export default {
+        name: "ContactUs",
+        components:{
+            ContactForm
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
